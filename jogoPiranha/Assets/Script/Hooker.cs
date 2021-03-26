22c18d7bda52317a05a723019fdc5a5b6ee8e1be
Maycon R Santana <maycon_gus2009@hotmail.com>
2021-03-25 21:06:43 -0300

    criando o script do personagem piranha

---
+++ jogoPiranha/Assets/Script/Hooker.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hooker : MonoBehaviour
+{
+    public GameObject enemy;
+    public float hookerRate;
+
+    private float nextHooker = 0f;
+
+    void Update()
+    {
+        if (Time.time > nextHooker)
+        {
+            nextHooker = Time.time + hookerRate;
+
+            Instantiate(enemy, transform.position, enemy.transform.rotation);
+        }
+    }
+}
